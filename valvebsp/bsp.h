3dcf94f6328236907e9118dafd0ff91627c9f614
norrbotten <axelsp@gmail.com>
2019-09-29 18:41:34 +0200

    moor lumps

--- valvebsp/bsp.h
+++ valvebsp/bsp.h
@@ -15,6 +15,9 @@ namespace ValveBSP {
 	constexpr unsigned int MAX_MAP_VERTS = 65536;	// Maximum amount of vertexes
 	constexpr unsigned int MAX_MAP_EDGES = 256000;	// Maximum amount of edges
 	constexpr unsigned int MAX_MAP_SURFEDGES = 512000;	// Maximum amount of surfedges
+	constexpr unsigned int MAX_MAP_FACES = 65536;	// Maximum amount of faces
+	constexpr unsigned int MAX_MAP_TEXDATAS = 2048;	// Maximum amount of texture datas
+	constexpr unsigned int MAX_MAP_NODES = 65536;	// Maximum amount of BSP nodes
 
 	// 3D vector
 	struct Vector {
@@ -75,8 +78,95 @@ namespace ValveBSP {
 		int		type;
 	};
 
-	// vertex, just a vector
+	// dnode_t struct
+	struct dnode_t {
+		int				planenum;	// index into plane array
+		int				children[2];// negative numbers are -(leafs + 1), not nodes
+		short			mins[3];	// for frustum culling
+		short			maxs[3];
+		unsigned short	firstface;	// index into face array
+		unsigned short	numfaces;	// counting both sides
+		short			area;		// If all leaves below this node are in the same area, then
+									// this is the area index. If not, this is -1.
+		short			paddding;	// pad to 32 bytes length
+	};
+
+	// dleaf_t struct
+	struct dleaf_t {
+		int				contents;		// OR of all brushes (not needed?)
+		short			cluster;		// cluster this leaf is in
+		short			area:9;			// area this leaf is in
+		short			flags:7;		// flags
+		short			mins[3];		// for frustum culling
+		short			maxs[3];
+		unsigned short	firstleafface;	// index into leaffaces
+		unsigned short	numleaffaces;
+		unsigned short	firstleafbrush;	// index into leafbrushes
+		unsigned short	numleafbrushes;
+		short			leafWaterDataID;// -1 for not in water
+	};
+
+	struct ColorRGBExp32 {
+		unsigned char r, g, b;
+		char exponent;
+	};
+
+	struct CompressedLightCube {
+		ColorRGBExp32 m_Color[6];
+	};
+
+	// dleaf_t struct for map versions 19 or earlier
+	struct dleaf_t_19
+	{
+		int				contents;		// OR of all brushes (not needed?)
+		short			cluster;		// cluster this leaf is in
+		short			area:9;			// area this leaf is in
+		short			flags:7;		// flags
+		short			mins[3];		// for frustum culling
+		short			maxs[3];
+		unsigned short	firstleafface;	// index into leaffaces
+		unsigned short	numleaffaces;
+		unsigned short	firstleafbrush;	// index into leafbrushes
+		unsigned short	numleafbrushes;
+		short			leafWaterDataID;// -1 for not in water
+		
+		CompressedLightCube	ambientLighting;	// Precaculated light info for entities.
+		short			padding;		// padding to 4-byte boundary
+	};
+
+	// dtexdata_t, texture data
+	struct dtexdata_t {
+		Vector	reflectivity;
+		int		nameStringTableID;
+		int		width;
+		int		height;
+		int		view_width;
+		int		view_height;
+	};
+
+	// dvertex_t, just a vector
 	typedef Vector dvertex_t;
+
+	// dface_t
+	struct dface_t {
+		unsigned short	planenum;		// the plane number
+		char			side;			// faces opposite to the node's plane direction
+		char			onNode;			// 1 of on node, 0 if in leaf
+		int				firstedge;		// index into surfedges
+		short			numedges;		// number of surfedges
+		short			texinfo;		// texture info
+		short			dispinfo;		// displacement info
+		char			surfaceFogVolumeID;	// ???
+		char			styles[4];		// switchable lighting info
+		int				lightofs;		// offset into lightmap lump
+		float			area;			// face area in units^2
+		int				LightmapTextureMinsInLuxels[2];	// texture lighting info
+		int				LightmapTextureSizeInLuxels[2];	// texture lighting info
+		int				origFace;		// original face this was split from
+		unsigned short	numPrims;		// primitives
+		unsigned short	firstPrimID;	// ???
+		unsigned int	smoothingGroups;// lightmap smoothing group
+	};
 
 	// dedge_t, two shorts which reference the vertex lump
 	struct dedge_t {
@@ -98,9 +188,23 @@ namespace ValveBSP {
 	// array of dplane_t structs
 	typedef std::vector<dplane_t> lump_planes;
 
+	// LUMP 2, texture data
+	// array of dtexdata_t structs
+	typedef std::vector<dtexdata_t> lump_texdata;
+
 	// LUMP 3, vertexes
 	// array of dvertex_t
 	typedef std::vector<dvertex_t> lump_vertexes;
+
+	// LUMP 5, bsp nodes
+	// array of dnote_t
+	typedef std::vector<dnode_t> lump_nodes;
+
+	// LUMP 7, faces
+	// array of dedge_t
+	typedef std::vector<dface_t> lump_faces;
+
+
 
 	// LUMP 12, edges
 	// array of dedge_t
@@ -118,7 +222,9 @@ namespace ValveBSP {
 
 		lump_entities	lump0_entities;
 		lump_planes		lump1_planes;
+		lump_texdata	lump2_texdata;
 		lump_vertexes	lump3_vertexes;
+		lump_faces		lump7_faces;
 		lump_edges		lump12_edges;
 		lump_surfedges	lump13_surfedges;
 	};
--- valvebsp/parser.h
+++ valvebsp/parser.h
@@ -78,6 +78,10 @@ namespace ValveBSP {
 		bool Parse() {
 			#define c(m) if (!m) return false
 
+			auto checkoffset = [&](size_t offset, size_t size) {
+				return (offset + size) < this->length;
+			};
+
 			// HEADER, since the header is always the fixed size we can just type-pun it
 			verboseprint("Parsing header.. ");
 			c(ReadType<dheader_t>(bsp.header));
@@ -90,11 +94,30 @@ namespace ValveBSP {
 			verboseprint("BSP File version: " << bsp.header.version << "\t");
 			verboseprint("Map revision: " << bsp.header.mapRevision << "\n");
 
+			// LUMP TODO:
+			/*
+				0	DONE	10			30			40			50			60
+				1	DONE	11			31			41			51			61
+				2	DONE	12	DONE	32			42			52			62
+				3	DONE	13	DONE	33			43			53			63
+				4			14			34			44			54
+				5			15			35			45			55
+				6			16			36			46			56
+				7	DONE	17			37			47			57
+				8			18			38			48			58
+				9			29			39			49			59
+			*/
+
 			// LUMP 0: Entities
 			{
 				verboseprint("Parsing lump 0 (entities)..\n");
-
+				
 				lump_t* lumpinfo = &bsp.header.lumps[0];
+
+				if (!checkoffset(lumpinfo->fileofs, lumpinfo->filelen)) {
+					std::cerr << "\nUnexpected EOF\n";
+					return false;
+				}
 
 				bsp.lump0_entities.reserve(lumpinfo->filelen);
 				memcpy((void*)bsp.lump0_entities.data(), this->buffer + lumpinfo->fileofs, lumpinfo->filelen);
@@ -107,6 +130,12 @@ namespace ValveBSP {
 				verboseprint("Parsing lump 1 (planes).. ");
 				
 				lump_t* lumpinfo = &bsp.header.lumps[1];
+
+				if (!checkoffset(lumpinfo->fileofs, lumpinfo->filelen)) {
+					std::cerr << "\nUnexpected EOF\n";
+					return false;
+				}
+				
 				size_t numPlanes = lumpinfo->filelen / sizeof(dplane_t);
 
 				if (numPlanes > MAX_MAP_PLANES) {
@@ -124,12 +153,47 @@ namespace ValveBSP {
 
 				bsp.lumps[1] = (void*)&bsp.lump1_planes;
 			}
+			
+			// LUMP 2: Texture data
+			{
+				verboseprint("Parsing lump 2 (texdata).. ");
+
+				lump_t* lumpinfo = &bsp.header.lumps[2];
+
+				if (!checkoffset(lumpinfo->fileofs, lumpinfo->filelen)) {
+					std::cerr << "\nUnexpected EOF\n";
+					return false;
+				}
+
+				size_t numTexdatas = lumpinfo->filelen / sizeof(dtexdata_t);
+
+				if (numTexdatas > MAX_MAP_TEXDATAS) {
+					std::cerr << "BSP has more than the allowed amount of texdatas\n";
+					return false;
+				}
+
+				for (int i = 0; i < numTexdatas; i++) {
+					dtexdata_t texdata;
+					memcpy((void*)&texdata, this->buffer + lumpinfo->fileofs + i * sizeof(dtexdata_t), sizeof(dtexdata_t));
+					bsp.lump2_texdata.push_back(texdata);
+				}
+
+				verboseprint(numTexdatas << " texdatas\n");
+
+				bsp.lumps[2] = (void*)&bsp.lump2_texdata;
+			}
 
 			// LUMP 3: VERTEXES
 			{
 				verboseprint("Parsing lump 3 (vertexes).. ");
 
 				lump_t* lumpinfo = &bsp.header.lumps[3];
+
+				if (!checkoffset(lumpinfo->fileofs, lumpinfo->filelen)) {
+					std::cerr << "\nUnexpected EOF\n";
+					return false;
+				}
+
 				size_t numVerts = lumpinfo->filelen / sizeof(dvertex_t);
 
 				if (numVerts > MAX_MAP_VERTS) {
@@ -148,11 +212,46 @@ namespace ValveBSP {
 				bsp.lumps[3] = (void*)&bsp.lump3_vertexes;
 			}
 
+			// LUMP 7: FACE AND ORIGINAL FACE
+			{
+				verboseprint("Parsing lump 7 (faces).. ");
+
+				lump_t* lumpinfo = &bsp.header.lumps[7];
+
+				if (!checkoffset(lumpinfo->fileofs, lumpinfo->filelen)) {
+					std::cerr << "\nUnexpected EOF\n";
+					return false;
+				}
+
+				size_t numFaces = lumpinfo->filelen / sizeof(dface_t);
+
+				if (numFaces > MAX_MAP_FACES) {
+					std::cerr << "BSP has more than the allowed amount of faces\n";
+					return false;
+				}
+
+				for (int i = 0; i < numFaces; i++) {
+					dface_t face;
+					memcpy((void*)&face, this->buffer + lumpinfo->fileofs + i * sizeof(dface_t), sizeof(dface_t));
+					bsp.lump7_faces.push_back(face);
+				}
+
+				verboseprint(numFaces << " faces\n");
+
+				bsp.lumps[7] = (void*)&bsp.lump7_faces;
+			}
+
 			// LUMP 12: EDGES
 			{
-				verboseprint("Parsing lump 12 (vertexes).. ");
+				verboseprint("Parsing lump 12 (edges).. ");
 
 				lump_t* lumpinfo = &bsp.header.lumps[12];
+
+				if (!checkoffset(lumpinfo->fileofs, lumpinfo->filelen)) {
+					std::cerr << "\nUnexpected EOF\n";
+					return false;
+				}
+
 				size_t numEdges = lumpinfo->filelen / sizeof(dedge_t);
 
 				if (numEdges > MAX_MAP_EDGES) {
@@ -171,11 +270,17 @@ namespace ValveBSP {
 				bsp.lumps[12] = (void*)&bsp.lump12_edges;
 			}
 
-			// LUMP 13: EDGES
+			// LUMP 13: SURFEDGES
 			{
 				verboseprint("Parsing lump 13 (surfedges).. ");
 
 				lump_t* lumpinfo = &bsp.header.lumps[13];
+
+				if (!checkoffset(lumpinfo->fileofs, lumpinfo->filelen)) {
+					std::cerr << "\nUnexpected EOF\n";
+					return false;
+				}
+
 				size_t numSurfedges = lumpinfo->filelen / sizeof(dsurfedge_t);
 
 				if (numSurfedges > MAX_MAP_SURFEDGES) {
@@ -197,8 +302,8 @@ namespace ValveBSP {
 			return true;
 			#undef c
 		}
-
+		
 		#undef verboseprint
 	};
-
+	
 }
